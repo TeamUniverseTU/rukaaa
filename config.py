fa1f4fafa49af7de7d1c94143a1c2d3753879b98
TeamUniversTU <138324953+TeamUniverseTU@users.noreply.github.com>
2023-10-30 00:57:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ APP_ID = int(os.environ.get("APP_ID", "27515671"))
 API_HASH = os.environ.get("API_HASH", "8fbf2fa788fdf1ecb424229bc836ffa8")
 
 # ID Channel Database
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001845718901"))
 
 # NAMA OWNER
 OWNER = os.environ.get("OWNER", "Aryan x TeamUniverse")
