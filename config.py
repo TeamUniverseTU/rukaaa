a9c81e6abc9e01038351a9111a2f941d5ebe2b58
TeamUniversTU <138324953+TeamUniverseTU@users.noreply.github.com>
2023-11-05 23:11:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6851564977:AAG8RXQu1bv9Tr5a03UYyWnDxAYB2Bvf8v4") 
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6380765721:AAH7IBAIPDpEC_m6XYPsqC9BjeXM-s5v9qM") 
 # API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "27515671"))
 
@@ -35,7 +35,7 @@ HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 
 # Database
-DB_URI = os.environ.get("DATABASE_URL", "postgres://bgmrjtfa:h_wLd2Yy6HlkVQHftCmHjLR6ggVvSIeZ@floppy.db.elephantsql.com/bgmrjtfa")
+DB_URI = os.environ.get("DATABASE_URL", "postgres://kvsdjrrj:FAbEb71nKTM_SyQuJ0HTbQBx9IXrP_Q2@floppy.db.elephantsql.com/kvsdjrrj")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001898669613")) 
